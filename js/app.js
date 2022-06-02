857abff84406f09c62772684761afa5ec34f3180
Kim Minseo <terrykms30@gmail.com>
2022-06-02 17:04:06 +0800

    added typed.js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+// typed.js
+var typed = new Typed("#typed", {
+    stringsElement: "#typed-strings",
+    backSpeed: 30,
+    typeSpeed: 30,
+    startDelay: 30,
+    loop: true
+});
+
+
+
+
+
+// window.onscroll = changeNavbarColor;
+
+// function changeNavbarColor() {
+//     var navbar = document.querySelector(".navbar");
+//     if (window.scrollY >= 100 ) {
+//         navbar.classList.add("navbar-color", "navbar-dark")
+//         navbar.classList.remove("bg-transparent", "navbar-light")
+//     } else {
+//         navbar.classList.add("bg-transparent", "navbar-light")
+//         navbar.classList.remove("navbar-color", "navbar-dark")
+//     }
+// }
+
+// function showNavbar() {
+//     var navbar = document.querySelector(".navbar");
+//     if (window.scrollY >= 100 ) {
+//         navbar.style.display = "block";
+//     } else {
+//         navbar.style.display = "none";
+//     }
+// }
