aea96e91c1aaa9a5542a1b4573d615d9ce3314ba
Kim Minseo <terrykms30@gmail.com>
2022-06-14 11:52:45 +0800

    added personal project

--- js/app.js
+++ js/app.js
@@ -24,24 +24,25 @@ function reveal() {
     })
 }
 
+// function showNavbar() {
+//     var navbar = document.querySelector(".navbar");
+//     if (window.scrollY >= 200 ) {
+//         navbar.classList.add("navbar-color");
+//         navbar.classList.remove("transparent-bg");
+//     } else {
+//         navbar.classList.add("transparent-bg");
+//         navbar.classList.remove("navbar-color");
+//     }
+// }
+
+// window.addEventListener("scroll", showNavbar)
 window.addEventListener("scroll", reveal)
 
 
 
 
 
-// window.onscroll = changeNavbarColor;
 
-// function changeNavbarColor() {
-//     var navbar = document.querySelector(".navbar");
-//     if (window.scrollY >= 100 ) {
-//         navbar.classList.add("navbar-color", "navbar-dark")
-//         navbar.classList.remove("bg-transparent", "navbar-light")
-//     } else {
-//         navbar.classList.add("bg-transparent", "navbar-light")
-//         navbar.classList.remove("navbar-color", "navbar-dark")
-//     }
-// }
 
 // function showNavbar() {
 //     var navbar = document.querySelector(".navbar");
